66f6839fc31f0e9403f536c80a804cd7c7c29848
ndavidson <ndavidson@7ca69b2b-3e8c-f36b-109c-63a388394f7f>
2011-01-11 03:37:04 +0000

    updating

--- examples/PlanarCDI_example.sh
+++ examples/PlanarCDI_example.sh
@@ -3,7 +3,7 @@
 # This shows how to run the planar reconstuction
 # using the command line tool. 
 
-../tools/planar_CDI_reconstruction.exe example.config
+../bin/planar_CDI_reconstruction.exe example.config
 
 #note: once the tools are installed properly you should only
 #need to run "planar_CDI_reconstruction example.config"
